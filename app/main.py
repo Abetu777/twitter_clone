42aa4c2bd463e6e4320fe1a79ad90bf2f8cc607c
Abe Tsubasa <abetu777@gmail.com>
2025-01-01 13:09:37 +0900

    Add root endpoint

--- app/main.py
+++ app/main.py
@@ -21,3 +21,7 @@ def create_post():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to the Twitter Clone API!"
