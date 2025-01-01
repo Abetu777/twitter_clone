66798b8ce45b290777848b36d5ea8dc20f2e331d
Abe Tsubasa <abetu777@gmail.com>
2025-01-01 14:09:48 +0900

    create html

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
 # 仮のデータベース
 posts = []
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to the Twitter Clone API!"
 
 @app.route("/posts", methods=["GET"])
 def get_posts():
@@ -18,11 +22,11 @@ def create_post():
     posts.append({"id": len(posts) + 1, "content": data["content"]})
     return jsonify(posts[-1]), 201
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Render用にポートを動的に設定
-    app.run(host="0.0.0.0", port=port)
+# 静的ファイルのホスティング
+@app.route("/static/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Welcome to the Twitter Clone API!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
