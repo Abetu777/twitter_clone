7be2a391c998570f60be4f32d4fde2ddf6d49556
Abe Tsubasa <abetu777@gmail.com>
2025-01-05 15:07:13 +0900

    U entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,17 @@
 # 環境変数に基づいてデータベースURIを設定
 if [ "$FLASK_ENV" = "production" ]; then
   echo "Running in production mode"
-  export DATABASE_URL=${DATABASE_URL:-"postgresql://user:password@db:5432/production_db"}
+  export DATABASE_URL=${DATABASE_URL:-"postgresql://user:password@db:5432/mydatabase"}
 else
   echo "Running in development mode"
   export DATABASE_URL="sqlite:///../instance/app.db"
 fi
+
+# データベースが利用可能になるまで待機
+until nc -z -v -w30 db 5432; do
+  echo "Waiting for database connection..."
+  sleep 1
+done
 
 # マイグレーションを実行
 flask db upgrade || echo "Database already upgraded"
