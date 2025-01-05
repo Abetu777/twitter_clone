b229a0d554a788110b9f40e091584ebf4b5b02ae
Abe Tsubasa <abetu777@gmail.com>
2025-01-05 17:40:35 +0900

    u

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# wait-for-db.shを使用して、DBが接続可能になるまで待機
 echo "Waiting for the database to be available..."
-until nc -z -v -w30 mydatabase 5432
+until nc -z -v -w30 db 5432 && psql -h db -U user -d mydatabase -c "SELECT 1;" 2>/dev/null
 do
   echo "Waiting for database connection..."
   sleep 1
 done
 echo "Database is up, starting the application..."
-# アプリケーションの起動コマンドを実行
 exec "$@"
